1321024779276a51514406d20d55235928daf2ac
yogi4004 <ishshan27@gmail.com>
2025-07-08 22:20:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 3000 });
+const PORT = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port: PORT });
+
 
 let clients = [];
 
